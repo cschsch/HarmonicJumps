e14ecd8432ff3552b05cd36559fbbcfe6648b47b
csch <cschwarzbeck@TECHNIK-CON>
2021-10-29 13:15:58 +0200

    Adding GUI Project

---
+++ GUI/Model/MainModel.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonicJumps;
+
+namespace GUI.Model
+{
+    public class MainModel
+    {
+        public Track NowPlaying { get; set; }
+        public Track[] Tracks { get; set; }
+        public Track[] FilteredTracks { get; set; }
+
+        public MainModel(Track[] tracks)
+        {
+            Tracks = tracks;
+        }
+    }
+
+}
