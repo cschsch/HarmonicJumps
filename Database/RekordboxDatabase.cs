3f3b754842412fc6bc0c25912c2fb802388bc126
csch <cschwarzbeck@TECHNIK-CON>
2021-10-29 13:10:19 +0200

    Adding SharedCache flag

--- Database/RekordboxDatabase.cs
+++ Database/RekordboxDatabase.cs
@@ -9,7 +9,7 @@ namespace Database
         public RekordboxDatabase(string path)
         {
             ConnectionString = new SQLiteConnectionString(path,
-                openFlags: SQLiteOpenFlags.ReadOnly,
+                openFlags: SQLiteOpenFlags.ReadOnly | SQLiteOpenFlags.SharedCache,
                 storeDateTimeAsTicks: true,
                 key: "402fd482c38817c35ffa8ffb8c7d93143b749e7d315df7a81732a1ff43608497");
         }
