38e770d1129724007baef98c77375a3f0f39fc7c
csch <cschwarzbeck@TECHNIK-CON>
2021-12-09 15:08:23 +0100

    Moving Configuration to HarmonicJumps

--- CommandLine/Program.cs
+++ CommandLine/Program.cs
@@ -1,25 +1,34 @@
 ﻿using System;
 using System.Linq;
 using System.Linq.Expressions;
+using Database;
 using GUI.Model;
 using HarmonicJumps;
+using Microsoft.Extensions.Configuration;
 using SQLite;
 
 namespace CommandLine
 {
+    //TODO currently only used for debugging purposes
     public class Program
     {
         static void Main(string[] args)
         {
-            var rekordboxDatabase = new Database.RekordboxDatabase("C:\\Users\\cschwarzbeck.HOME\\Desktop\\rekordbox_bak_20210916\\master.db");
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .AddCommandLine(Environment.GetCommandLineArgs())
+                .Build()
+                .Get<Configuration>();
+
+            var rekordboxDatabase = new RekordboxDatabase(configuration.DatabasePath);
             using var db = new SQLiteConnection(rekordboxDatabase.ConnectionString);
-            var tracks = db.Table<Database.Content>().AsParallel().Select(c => Track.FromID(db, c.ID)).ToArray();
-            var harmonizer = new Harmonizer(4);
+            var tracks = db.Table<Content>().AsParallel().Select(c => Track.FromID(db, c.ID)).ToArray();
+            var harmonizer = new Harmonizer(configuration.HarmonizerDepth);
             var trackFinder = new TrackFinder(harmonizer, tracks);
 
             var path = trackFinder.Find(
-                tracks.First(track => track.Key == Key.Create(4, Signature.Major)),
-                tracks.First(track => track.Key == Key.Create(12, Signature.Minor)))
+                tracks.First(track => track.Key == HarmonicJumps.Key.Create(4, Signature.Major)),
+                tracks.First(track => track.Key == HarmonicJumps.Key.Create(12, Signature.Minor)))
                 .Take(5)
                 .ToArray();
         }
--- HarmonicJumps/Configuration.cs
+++ HarmonicJumps/Configuration.cs
@@ -1,4 +1,4 @@
-﻿namespace GUI
+﻿namespace HarmonicJumps
 {
     public class Configuration
     {
